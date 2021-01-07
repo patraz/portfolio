a27e36f10520e583ead8c1cf722df542da734532
Patryk <patrykprause@gmail.com>
2021-01-07 21:49:30 +0100

    changes

---
+++ script.js
@@ -0,0 +1,24 @@
+const omnie = document.getElementById("omnie");
+const projekty = document.getElementById("projekty");
+const kontakt = document.getElementById('kontakt');
+const li = document.querySelector('li')
+
+
+const omnieBtn = document.getElementById('omniebtn');
+const projektyBtn = document.getElementById('projektybtn');
+const kontaktBtn = document.querySelector('.kontaktbtn');
+
+console.log(projektyBtn)
+
+
+omnieBtn.addEventListener('click', () => {
+    omnie.scrollIntoView();
+});
+
+projektyBtn.addEventListener('click', () => {
+    projekty.scrollIntoView();
+});
+
+kontaktBtn.addEventListener('click', () => {
+    kontakt.scrollIntoView();
+});
